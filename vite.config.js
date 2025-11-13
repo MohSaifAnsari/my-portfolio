618c86352356fa69557d216e6ceb30f8a59393a3
Moh Saif Ansari <saifansari50231@gmail.com>
2025-11-13 19:43:36 +0530

    Initial commit for portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/',  
   plugins: [react()],
-  server: {
-    open: 'chrome',
-  }
+  base: '/my-portfolio/',
 })
